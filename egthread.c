73c41de4c166c621006115bd0d18868d6d4402b9
Kevin Hwang <neoqmin@gmail.com>
2015-10-12 12:29:56 +0900

    First commit

---
+++ egthread.c
@@ -0,0 +1,40 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int sum;
+void *runner(void *param);
+
+int main(int argc, char *argv[])
+{
+	pthread_t tid;
+	pthread_attr_t attr;
+
+	if (argc != 2)
+	{
+		return -1;
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		return -1;
+	}
+	pthread_attr_init(&attr);
+
+	pthread_create(&tid, &attr, runner, argv[1]);
+
+	pthread_join(tid, NULL);
+
+	printf("sum = %d\n",  sum);
+
+}
+
+void *runner(void *param)
+{
+	int i, upper = atoi(param);
+	sum = 0;
+
+	for (i = 1;i <= upper;i++)
+	{
+		sum+=i;
+	}
+	pthread_exit(0);
+}
